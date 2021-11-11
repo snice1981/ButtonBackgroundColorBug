2b302c70834bdc9c88d80b98adbfd2471e6a4f14
Mohammed Messaoudi <mohammed.messaoudi0@gmail.com>
2021-11-11 10:39:59 +0100

    changed buttons

--- TestApp/TestApp/ViewModels/AboutViewModel.cs
+++ TestApp/TestApp/ViewModels/AboutViewModel.cs
@@ -7,17 +7,21 @@ namespace TestApp.ViewModels
 {
     public class AboutViewModel : BaseViewModel
     {
-        public bool SubscribedSession { get; set; }
+        public bool Enabled { get; set; }
         public AboutViewModel()
         {
             Title = "About";
             OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
-            ShareCommand = new Command(async () => await Shell.Current.DisplayAlert("hi", "test message", "cancel"), () => SubscribedSession);
-            ShareCommand2 = new Command(async () => await Shell.Current.DisplayAlert("hi", "test message", "cancel"), () => true);
+            Button1Command = new Command(async () => await Shell.Current.DisplayAlert("Button 1", "Button 1", "Cancel"), () => false);
+            Button2Command = new Command(async () => await Shell.Current.DisplayAlert("Button 2", "Button 2", "Cancel"), () => true);
+            Button3Command = new Command(async () => await Shell.Current.DisplayAlert("Button 3", "Button 3", "Cancel"), () => Enabled);
+            ToggleEnableCommand = new Command(() => { Enabled = !Enabled; Button3Command.ChangeCanExecute(); });
         }
 
-        public ICommand OpenWebCommand { get; }
-        public ICommand ShareCommand { get; }
-        public ICommand ShareCommand2 { get; }
+        public Command OpenWebCommand { get; }
+        public Command Button1Command { get; }
+        public Command Button2Command { get; }
+        public Command Button3Command { get; }
+        public Command ToggleEnableCommand { get; }
     }
 }
